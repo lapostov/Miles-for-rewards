cf98f625c2514b25621b1ddf130d364ebc721b9a
Александр Масляков <lapostov@yandex.ru>
2022-07-06 16:54:51 +0400

    last commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+
+    public int calculate(int price) {
+    int miles = price / 20;
+    return miles;
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class miles_for_rewards {
+public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 1_000_000_000;
