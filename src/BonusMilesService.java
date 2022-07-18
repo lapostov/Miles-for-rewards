aef9a2671d804bd05933e564297018aacf12fef3
Александр Масляков <lapostov@yandex.ru>
2022-07-18 16:57:18 +0400

    Added reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-    int miles = price / 20;
-    return miles;
+        int miles = price / 20;
+        return miles;
     }
 
 }
